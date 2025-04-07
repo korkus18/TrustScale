1b2c9858d1715031dcf7eab082fe649c16e939ff
illya <illyamiloserdov@skaut.cz>
2025-04-07 10:39:55 +0200

    chore: Push to save

---
+++ app/helpers/analyzer.py
@@ -0,0 +1,12 @@
+from app.models.post import Post
+from app.models.result import Result
+from app.helpers.scraper import Scraper
+from requests import get, post
+
+
+class Analyzer:
+    def __init__(self):
+        pass
+
+    def run(self, post: Post) -> Result:
+        post =
---
+++ app/helpers/scraper.py
@@ -0,0 +1,10 @@
+from app.models.post import Post
+from requests import post
+
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    def scrape(self, url: str) -> Post:
+        called_post =
